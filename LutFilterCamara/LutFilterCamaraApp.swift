9167bfc07770838df9abe75d34cfd0324a2a91e9
김주희 <juhee1687@naver.com>
2024-01-27 14:27:23 +0900

    init

---
+++ LutFilterCamara/LutFilterCamaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LutFilterCamaraApp.swift
+//  LutFilterCamara
+//
+//  Created by 김주희 on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LutFilterCamaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
